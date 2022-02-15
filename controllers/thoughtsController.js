71b1400807fc2e48e513552281b40de1e2d09c29
Samuel Sholib <samuelsholib12@gmail.com>
2022-02-14 22:40:08 -0800

    get route controllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,14 +1,14 @@
 const { Thought, User } = require('../models')
 
 const thoughtController = {
-    //get all thoughts
+    // get all thoughts
     getAllThoughts(req, res) {
         Thought.find({})
-        //.populate({
-        //     path: 'user',
+        // .populate({
+        //     path: 'thoughts',
         //     select: '-__v'
         // })
-        // .select('-__v')
+        .select('-__v')
         .sort({ _id: -1 })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
@@ -17,13 +17,15 @@ const thoughtController = {
         });
     },
 
+
+
     //get thought by ID
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.id })
-            .populate({
-                path: 'user',
-                select: '-__v'
-            })
+            // .populate({
+            //     path: 'user',
+            //     select: '-__v'
+            // })
            .select('-__v')
            .sort({ _id: -1 })
            .then(dbThoughtData => res.json(dbThoughtData))
